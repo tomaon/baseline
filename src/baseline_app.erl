01f5b7277437924cf1fa5f533c1efcc655169a09
tomaon <tm.aono+b@gmail.com>
2015-01-09 09:58:16 +0900

    upd: copyright

--- src/baseline_app.erl
+++ src/baseline_app.erl
@@ -1,5 +1,5 @@
 %% =============================================================================
-%% Copyright 2014 AONO Tomohiko
+%% Copyright 2014-2015 AONO Tomohiko
 %%
 %% This library is free software; you can redistribute it and/or
 %% modify it under the terms of the GNU Lesser General Public
--- src/baseline_socket.erl
+++ src/baseline_socket.erl
@@ -1,4 +1,18 @@
 %% =============================================================================
+%% Copyright 2014-2015 AONO Tomohiko
+%%
+%% This library is free software; you can redistribute it and/or
+%% modify it under the terms of the GNU Lesser General Public
+%% License version 2.1 as published by the Free Software Foundation.
+%%
+%% This library is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%% Lesser General Public License for more details.
+%%
+%% You should have received a copy of the GNU Lesser General Public
+%% License along with this library; if not, write to the Free Software
+%% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 %% =============================================================================
 
 -module(baseline_socket).
@@ -112,8 +126,7 @@ call(#handle{}=H, Packet, Term, Timeout) ->
 dispatch(Socket, Binary, Term, Timeout)
   when is_integer(Term) ->
     recv_binary(Socket, Binary, Term, Timeout);
-dispatch(Socket, Binary, Term, Timeout)
-  -> % when is_tuple(Term) -> % {bm,_}
+dispatch(Socket, Binary, Term, Timeout) ->
     recv_text(Socket, Binary, Term, Timeout).
 
 recv_binary(Socket, Binary, Size, Timeout) ->
